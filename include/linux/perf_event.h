ae2ff2c38abf58e48536858036afb53ea92980df
Ben Hutchings <ben@decadent.org.uk>
2016-01-11 15:23:55 +0000

    security,perf: Allow further restriction of perf_event_open
    When kernel.perf_event_open is set to 3 (or greater), disallow all
    access to performance events by users without CAP_SYS_ADMIN.
    Add a Kconfig symbol CONFIG_SECURITY_PERF_EVENTS_RESTRICT that
    makes this value the default.
    
    This is based on a similar feature in grsecurity
    (CONFIG_GRKERNSEC_PERF_HARDEN).  This version doesn't include making
    the variable read-only.  It also allows enabling further restriction
    at run-time regardless of whether the default is changed.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Albert I <kras@raphielgang.org>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1283,6 +1283,11 @@ static inline int perf_is_paranoid(void)
 	return sysctl_perf_event_paranoid > -1;
 }
 
+static inline bool perf_paranoid_any(void)
+{
+	return sysctl_perf_event_paranoid > 2;
+}
+
 static inline int perf_allow_kernel(struct perf_event_attr *attr)
 {
 	if (sysctl_perf_event_paranoid > 1 && !capable(CAP_SYS_ADMIN))
