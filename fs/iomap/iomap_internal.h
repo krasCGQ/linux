c8934e8fa92f523d108773b7491f0213ae82f188
Darrick J. Wong <darrick.wong@oracle.com>
2019-06-27 17:28:43 -0700

    iomap: move the direct IO code into a separate file
    Move the direct IO code into a separate file so that we can group
    related functions in a single file instead of having a single enormous
    source file.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

---
+++ fs/iomap/iomap_internal.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2010 Red Hat, Inc.
+ * Copyright (c) 2016-2018 Christoph Hellwig.
+ */
+#ifndef _IOMAP_INTERNAL_H_
+#define _IOMAP_INTERNAL_H_
+
+sector_t iomap_sector(struct iomap *iomap, loff_t pos);
+
+#endif /* _IOMAP_INTERNAL_H_ */
