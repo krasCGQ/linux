61535aa9a76c4fc92cc9b26340e5befc5dd4c777
Darrick J. Wong <darrick.wong@oracle.com>
2019-06-27 17:28:44 -0700

    iomap: move the buffered write code into a separate file
    Move the buffered write code into a separate file so that we can group
    related functions in a single file instead of having a single enormous
    source file.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/iomap/iomap_internal.h
+++ fs/iomap/iomap_internal.h
@@ -7,5 +7,11 @@
 #define _IOMAP_INTERNAL_H_
 
 sector_t iomap_sector(struct iomap *iomap, loff_t pos);
+void iomap_set_range_uptodate(struct page *page, unsigned off, unsigned len);
+struct iomap_page *iomap_page_create(struct inode *inode, struct page *page);
+void iomap_adjust_read_range(struct inode *inode, struct iomap_page *iop,
+		loff_t *pos, loff_t length, unsigned *offp, unsigned *lenp);
+void iomap_read_inline_data(struct inode *inode, struct page *page,
+		struct iomap *iomap);
 
 #endif /* _IOMAP_INTERNAL_H_ */
