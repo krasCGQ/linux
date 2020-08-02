66c8a688447b715f60ded1789f373949c5cfedc3
Alfred Chen <cchalpha@gmail.com>
2020-08-02 20:27:29 +0800

    sched/alt: [Sync] 739f70b476cf sched/core: s/WF_ON_RQ/WQ_ON_CPU/
    Signed-off-by: Albert I <kras@raphielgang.org>

--- kernel/sched/alt_sched.h
+++ kernel/sched/alt_sched.h
@@ -70,7 +70,7 @@ static inline int task_on_rq_migrating(struct task_struct *p)
 #define WF_SYNC		0x01		/* waker goes to sleep after wakeup */
 #define WF_FORK		0x02		/* child wakeup after fork */
 #define WF_MIGRATED	0x04		/* internal use, task got migrated */
-#define WF_ON_RQ	0x08		/* Wakee is on_rq */
+#define WF_ON_CPU	0x08		/* Wakee is on_rq */
 
 /*
  * This is the main, per-CPU runqueue data structure.
