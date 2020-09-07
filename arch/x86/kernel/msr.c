4ecbe518bc692f3e5f6089413dafede0c7d21eab
Jason A. Donenfeld <Jason@zx2c4.com>
2020-09-07 11:36:54 +0200

    x86/msr: do not warn on writes to OC_MAILBOX
    Popular tools, like intel-undervolt, use MSR 0x150 to control the CPU
    voltage offset. In fact, evidently the intel_turbo_max_3 driver in-tree
    also uses this MSR. So, teach the kernel's MSR list about this, so that
    intel-undervolt and other such tools don't spew warnings to dmesg, while
    unifying the constant used throughout the kernel.
    
    Fixes: a7e1f67ed29f ("x86/msr: Filter MSR writes")
    Cc: Borislav Petkov <bp@suse.de>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Albert I <kras@raphielgang.org>

--- arch/x86/kernel/msr.c
+++ arch/x86/kernel/msr.c
@@ -99,8 +99,11 @@ static int filter_write(u32 reg)
 	if (!__ratelimit(&fw_rs))
 		return 0;
 
-	if (reg == MSR_IA32_ENERGY_PERF_BIAS)
+	switch (reg) {
+	case MSR_IA32_ENERGY_PERF_BIAS:
+	case MSR_IA32_OC_MAILBOX:
 		return 0;
+	}
 
 	pr_err("Write to unrecognized MSR 0x%x by %s (pid: %d). Please report to x86@kernel.org.\n",
 	       reg, current->comm, current->pid);
